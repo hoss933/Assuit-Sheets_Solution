2e332e40b267b6be6a899d25b69e4a0c0909d14c
Hossam Reda <rhossam600@gmail.com>
2025-02-17 17:48:13 +0200

    Sheet_4

---
+++ #Sheet_4/A. Create A New String.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char S[1001];
+
+    // Read input
+    scanf("%s", S);
+
+    // Print the length using strlen()
+    printf("%lu\n", strlen(S));
+
+    return 0;
+}
